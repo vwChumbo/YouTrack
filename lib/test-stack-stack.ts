da7bbcf701bec7e6abcef8c21988c3845e24a9b0
José Chumbo <162104829+vwChumbo@users.noreply.github.com>
2026-04-13 11:08:25 +0100

    Revert "Remove Lambda-dependent features from CDK stack"
    This reverts commit ed89d89b3ac5723f9413d20f27b576aa3e675e5d.

--- lib/test-stack-stack.ts
+++ lib/test-stack-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class TestStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create S3 bucket
+    // Create S3 bucket with auto-delete enabled for easy cleanup
     const bucket = new s3.Bucket(this, 'TestBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
       encryption: s3.BucketEncryption.S3_MANAGED,
       versioned: false,
+    });
+
+    // Deploy assets to the bucket
+    new s3deploy.BucketDeployment(this, 'DeployAssets', {
+      sources: [s3deploy.Source.asset('./assets')],
+      destinationBucket: bucket,
     });
 
     // Output the bucket name
